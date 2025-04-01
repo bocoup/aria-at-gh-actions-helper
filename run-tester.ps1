88e3d807ae6ab78aed17469d195e082878d8574d
Mike Pennisi <mike@mikepennisi.com>
2025-04-01 16:15:03 -0400

    Enable Chrome logs

--- run-tester.ps1
+++ run-tester.ps1
@@ -50,7 +50,7 @@ switch ($env:BROWSER)
   chrome
   {
     Write-Output "Starting chromedriver"
-    $webdriverprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -ScriptBlock { chromedriver --port=4444 --log-level=INFO *>&1 >$using:loglocation\webdriver.log }
+    $webdriverprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -ScriptBlock { chromedriver --port=4444 --log-level=INFO --enable-chrome-logs *>&1 >$using:loglocation\webdriver.log }
     Write-Output "Waiting for localhost:4444 to start from chromedriver"
     Wait-For-HTTP-Response -RequestURL http://localhost:4444/
     Break
